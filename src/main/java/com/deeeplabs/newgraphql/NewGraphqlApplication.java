9a8187047c5693b7a465816a64710c3d2d1dd50b
Johannnes Agus <65710508+J-Agus96@users.noreply.github.com>
2021-06-23 10:34:47 +0700

    Initial commit

---
+++ src/main/java/com/deeeplabs/newgraphql/NewGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.deeeplabs.newgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deeeplabs/newgraphql/NewGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deeeplabs.newgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
